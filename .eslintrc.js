97916a8762a48d132b6a83cba1f68ced87d2a271
singcl <24661881@qq.com>
2018-04-08 11:25:40 +0800

    :wrench: Add eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": ["error",4],
+        "quotes": ["error","single"],
+        "semi": ["error", "never"],
+        "no-console": 0
+    }
+};
