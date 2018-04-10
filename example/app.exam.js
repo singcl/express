a46052d52069dcbdcefcca6441318c02b3ff12d4
singcl <24661881@qq.com>
2018-04-10 19:13:48 +0800

    :memo: example update

--- example/app.exam.js
+++ example/app.exam.js
@@ -19,6 +19,15 @@ app.get('/user', function(req, res, next) {
     })
 })
 
+app.post('/user', function(req, res, next) {
+    // 告诉客户端以UTF-8的方式解析 设置charset=utf-8解决前端拿到的数据中文乱码
+    res.setHeader('Content-Type', 'text/plain;charset=utf-8')
+    res.write('来自星星的POST', 'utf8')
+    res.end(function() {
+        console.log('post数据已发送！')
+    })
+})
+
 // 错误中间件use - 自定义如何处理错误
 app.use(error(function(err, req, res, next) {
     console.log(err)
