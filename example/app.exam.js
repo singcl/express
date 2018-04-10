83568139660e50cfcc267471c81da5bae29df306
singcl <24661881@qq.com>
2018-04-10 12:04:21 +0800

    :art: optimize app.example

--- example/app.exam.js
+++ example/app.exam.js
@@ -4,11 +4,15 @@ const Express = require('../src/application-hoc')
 const app = new Express()
 
 app.use('/example', function(req, res) {
-    res.write('来自星星的你')
+    // 告诉客户端以UTF-8的方式解析
+    // 设置charset=utf-8解决前端拿到的数据中文乱码
+    res.setHeader('Content-Type', 'text/plain;charset=utf-8')
+    res.write('来自星星的你', 'utf8')
     res.end(function() {
         console.log('数据已发送！')
     })
 })
+
 // 
 app.listen(3000, function() {
     console.log('node server start at port 3000!')
