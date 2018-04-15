7f01db0f1b2892fe2521d108b5774b6dffe1d41e
singcl <24661881@qq.com>
2018-04-15 19:21:07 +0800

    :art: update example

--- example/app.exam.js
+++ example/app.exam.js
@@ -7,6 +7,12 @@ var app = new Express()
 app.use(function(req, res, next) {
     // 告诉客户端以UTF-8的方式解析 设置charset=utf-8解决前端拿到的数据中文乱码
     res.setHeader('Content-Type', 'text/plain;charset=utf-8')
+    next('全局中间件错误捕获：next(err)')
+})
+
+app.get('/example', function(req, res, next) {
+    // 告诉客户端以UTF-8的方式解析 设置charset=utf-8解决前端拿到的数据中文乱码
+    res.setHeader('Content-Type', 'text/plain;charset=utf-8')
     next('路由中间件错误捕获：next(err)')
 })
 
