174004693049cab260c29147d6ae5620490ea28d
singcl <24661881@qq.com>
2018-04-15 19:18:25 +0800

    :art: update example with global middleware error situation

--- example/app.exam.js
+++ example/app.exam.js
@@ -4,7 +4,7 @@ var error = require('../src/middleware/error')
 var Express = require('../src/application')
 var app = new Express()
 
-app.get('/example', function(req, res, next) {
+app.use(function(req, res, next) {
     // 告诉客户端以UTF-8的方式解析 设置charset=utf-8解决前端拿到的数据中文乱码
     res.setHeader('Content-Type', 'text/plain;charset=utf-8')
     next('路由中间件错误捕获：next(err)')
