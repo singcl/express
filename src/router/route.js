5a66ea8de93d7db95b3297cea1965d5f39efda57
singcl <24661881@qq.com>
2018-04-18 01:23:31 +0800

    :art:  the second compose functional. The core code.the second next()

--- src/router/route.js
+++ src/router/route.js
@@ -43,29 +43,29 @@ methods.forEach(function(method) {
     return this
 })
 
-/**
- * @desc 客户端发起http请求后，通过该方法实现了两层Layer的遍历
- * @function
- * @name Route#dispatch
- * @param {Object}  req     请求流
- * @param {Object}  res     响应流
- * @param {Object}  out     控制权转移
- */
-Route.prototype.dispatch = function(req, res, out) {
-    var index = 0
-    var self = this
-    function next(err) {
-        if (err) return out(err)
-        if (index >= self.stack.length) return out()
-        var layer = self.stack[index++]
-        if (layer.method === req.method.toLowerCase()) {
-            layer.handleRequest(req, res, next)
-        } else {
-            next()
-        }
-    }
-    next() 
-}
+// /**
+//  * @desc 客户端发起http请求后，通过该方法实现了两层Layer的遍历
+//  * @function
+//  * @name Route#dispatch
+//  * @param {Object}  req     请求流
+//  * @param {Object}  res     响应流
+//  * @param {Object}  out     控制权转移
+//  */
+// Route.prototype.dispatch = function(req, res, out) {
+//     var index = 0
+//     var self = this
+//     function next(err) {
+//         if (err) return out(err)
+//         if (index >= self.stack.length) return out()
+//         var layer = self.stack[index++]
+//         if (layer.method === req.method.toLowerCase()) {
+//             layer.handleRequest(req, res, next)
+//         } else {
+//             next()
+//         }
+//     }
+//     next() 
+// }
 
 /**
  * Express Route Constructor module
