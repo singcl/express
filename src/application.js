97438b78119629c3da40338bccf56f6d4252cad9
singcl <24661881@qq.com>
2018-04-18 01:28:01 +0800

    :memo: Add more information about author.

--- src/application.js
+++ src/application.js
@@ -1,6 +1,7 @@
 /*!
  * @singcl/express
  * Copyright(c) 2018-2018 singcl
+ * github: https://github.com/singcl/express
  * BSD-3 Licensed
  */
 
--- src/express.js
+++ src/express.js
@@ -1,3 +1,10 @@
+/*!
+ * @singcl/express
+ * Copyright(c) 2018-2018 singcl
+ * github: https://github.com/singcl/express
+ * BSD-3 Licensed
+ */
+
 const Application = require('./application')
 
 // exports function that return instance of Application
