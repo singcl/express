360774f580e930862846b3773f8fabd327856064
singcl <24661881@qq.com>
2018-04-18 17:45:37 +0800

    :construction_worker: add mocha test runner.

---
+++ test/app.js
@@ -0,0 +1,25 @@
+var assert = require('assert')
+var express = require('..')
+var request = require('supertest')
+
+describe('app', function() {
+    // 测试@singcl/express 是否继承了Event Emitter
+    it('should inherit from event emitter', function(done) {
+        var app = express()
+        app.on('foo', done)
+        app.emit('foo')
+    })
+
+    // callable
+    it('should be callable', function() {
+        var app = express()
+        assert.equal(typeof app, 'function')
+    })
+
+    // 404
+    it('should 404 without routes', function(done) {
+        request(express())
+            .get('/')
+            .expect(404, done)
+    })
+})
